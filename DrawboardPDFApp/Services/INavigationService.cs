4b87df394f34d940cb31dc1c7db22a06fb12b719
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-02-27 18:59:02 +0200

    Create NavigationService and interface for it.

---
+++ DrawboardPDFApp/Services/INavigationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawboardPDFApp.Services
+{
+    public interface INavigationService
+    {
+        void Navigate(Type pageType);
+        void Navigate(Type pageType, object parameter);
+        void GoBack();
+        bool CanGoBack { get; }
+    }
+}
