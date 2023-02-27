a188f6a6d208bd52f85618a23cfa1e345631c1c1
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-02-27 19:41:47 +0200

    Create balic page that displays the content of pdf file

---
+++ DrawboardPDFApp/ViewModels/OpenedPdfViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Pdf;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace DrawboardPDFApp.ViewModels
+{
+    public class OpenedPdfViewModel : ObservableObject
+    {
+        public async Task CreatePdfPagesFromFileAsync(StorageFile pdfFile)
+        {
+            PdfPages.Clear();
+
+            PdfDocument pdfDocument = await PdfDocument.LoadFromFileAsync(pdfFile);
+
+            for (uint i = 0; i < pdfDocument.PageCount; i++)
+            {
+                var page = pdfDocument.GetPage(i);
+                var image = await ConvertPdfPageToBitmapImageAsync(page);
+                PdfPages.Add(image);
+            }
+        }
+
+        private async Task<BitmapImage> ConvertPdfPageToBitmapImageAsync(PdfPage page)
+        {
+            var image = new BitmapImage();
+            using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream())
+            {
+                await page.RenderToStreamAsync(stream);
+                await image.SetSourceAsync(stream);
+            }
+            return image;
+        }
+
+        public ObservableCollection<BitmapImage> PdfPages { get; set; } = new ObservableCollection<BitmapImage>();
+    }
+}
---
+++ DrawboardPDFApp/Views/OpenedPdfView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using DrawboardPDFApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DrawboardPDFApp.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class OpenedPdfView : Page
+    {
+        public OpenedPdfViewModel ViewModel { get; set; }
+
+        public OpenedPdfView()
+        {
+            ViewModel = new OpenedPdfViewModel();
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter is StorageFile pdfFile)
+            { 
+                await ViewModel.CreatePdfPagesFromFileAsync(pdfFile);
+            }
+            base.OnNavigatedTo(e);
+        }
+    }
+}
