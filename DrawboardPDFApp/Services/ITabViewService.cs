3355353679301bccccc6a62a87b647a93e404f67
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-02-28 22:04:45 +0200

    Create TabViewService and an interface for it

---
+++ DrawboardPDFApp/Services/ITabViewService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DrawboardPDFApp.Services
+{
+    public interface ITabViewService
+    {
+        void AddTab(string header, Type sourcePageType, object parameter);
+    }
+}
---
+++ DrawboardPDFApp/Services/TabViewService.cs
@@ -0,0 +1,35 @@
+﻿using DrawboardPDFApp.Views;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace DrawboardPDFApp.Services
+{
+    public class TabViewService : ITabViewService
+    {
+        private readonly TabView tabView;
+
+        public TabViewService(TabView tabView)
+        {
+            this.tabView = tabView;
+        }
+
+        public void AddTab(string header, Type sourcePageType, object parameter)
+        {
+            var newTab = new TabViewItem
+            {
+                Header = header
+            };
+
+            Frame frame = new Frame();
+            newTab.Content = frame;
+            frame.Navigate(sourcePageType, parameter);
+            tabView.TabItems.Add(newTab);
+            tabView.SelectedItem = newTab;
+        }
+    }
+}
