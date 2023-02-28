60eb9596b969699984de089894201c4f96d52737
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-02-28 22:03:03 +0200

    Create PdfFileOpenPicker service and an interface for it

---
+++ DrawboardPDFApp/Services/PdfFileOpenPicker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace DrawboardPDFApp.Services
+{
+    public class PdfFileOpenPicker : IPdfFileOpenPicker
+    {
+        private readonly FileOpenPicker fileOpenPicker;
+
+        public PdfFileOpenPicker()
+        {
+            fileOpenPicker = new FileOpenPicker();
+            fileOpenPicker.ViewMode = PickerViewMode.Thumbnail;
+            fileOpenPicker.FileTypeFilter.Add(".pdf");
+        }
+
+        public async Task<StorageFile> PickSingleFileAsync()
+        {
+            var storageFile = await fileOpenPicker.PickSingleFileAsync();
+            return storageFile;
+        }
+    }
+}
