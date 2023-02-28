c8867077ac078b911852f53773a8a634615b4827
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-02-28 09:35:09 +0200

    Create HomeView with basic functionality

---
+++ DrawboardPDFApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DrawboardPDFApp.Services;
+using DrawboardPDFApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xml.Linq;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml.Controls;
+
+namespace DrawboardPDFApp.ViewModels
+{
+    public class HomeViewModel : ObservableObject
+    {
+        private readonly INavigationService navigationService;
+
+        public HomeViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            OpenPdfFromDeviceCommand = new AsyncRelayCommand(OpenPdfFromDeviceAsync);
+        }
+
+        private int allFilesNumber;
+        public int AllFilesNumber
+        {
+            get => allFilesNumber;
+            set => SetProperty(ref allFilesNumber, value);
+        }
+
+        private int cloudFilesNumber;
+        public int CloudFilesNumber
+        {
+            get => cloudFilesNumber;
+            set => SetProperty(ref cloudFilesNumber, value);
+        }
+
+        public IAsyncRelayCommand OpenPdfFromDeviceCommand { get; }
+
+        private async Task OpenPdfFromDeviceAsync()
+        {
+            var picker = new FileOpenPicker();
+            picker.ViewMode = PickerViewMode.Thumbnail;
+            picker.FileTypeFilter.Add(".pdf");
+
+            var file = await picker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                navigationService.Navigate(typeof(OpenedPdfView), file);
+            }
+        }
+    }
+}
---
+++ DrawboardPDFApp/Views/HomeView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using DrawboardPDFApp.Services;
+using DrawboardPDFApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DrawboardPDFApp.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HomeView : Page
+    {
+        public HomeViewModel ViewModel { get; set; }
+
+        public HomeView()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ViewModel = new HomeViewModel(new NavigationService(Frame));
+            base.OnNavigatedTo(e);
+        }
+
+        private void Button_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            Window.Current.CoreWindow.PointerCursor = new CoreCursor(CoreCursorType.Hand, 0);
+        }
+
+        private void Button_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            Window.Current.CoreWindow.PointerCursor = new CoreCursor(CoreCursorType.Arrow, 1);
+        }
+    }
+}
