ea26a056909d8cb35dfdf65acd599c6db64ea784
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-03-02 20:03:45 +0200

    Create ApplicationContext for working with database

---
+++ DrawboardPDFApp/Repository/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using DrawboardPDFApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace DrawboardPDFApp.Repository
+{
+    public class ApplicationContext : DbContext, IApplicationContext
+    {
+        public DbSet<PdfFileInfo> OpenedPdfFilesHistory => Set<PdfFileInfo>();
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PdfFileInfo>().Property(x => x.Id).HasDefaultValue(Guid.NewGuid());
+        }
+    }
+}
---
+++ DrawboardPDFApp/Repository/IApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using DrawboardPDFApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DrawboardPDFApp.Repository
+{
+    
+    public interface IApplicationContext
+    {
+        DbSet<PdfFileInfo> OpenedPdfFilesHistory { get; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
