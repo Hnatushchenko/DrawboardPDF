b8713cf75ae80f1aadcd87a5a20217de2bb71dab
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-03-02 20:02:03 +0200

    Create model PdfFileInfo that will be stored in database and represents a file that has been opened by DrawboardPDF

---
+++ DrawboardPDFApp/Models/PdfFileInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawboardPDFApp.Models
+{
+    public class PdfFileInfo
+    {
+        public PdfFileInfo(string firstPageImagePath, string displayName, DateTimeOffset lastTimeOpened, string path)
+        {
+            FirstPageImagePath = firstPageImagePath;
+            DisplayName = displayName;
+            LastTimeOpened = lastTimeOpened;
+            Path = path;
+        }
+
+        public Guid Id { get; set; }
+        public string FirstPageImagePath { get; set; }
+        public string DisplayName { get; set; }
+        public DateTimeOffset LastTimeOpened { get; set; }
+        public string Path { get; set; }
+    }
+}
