af0fcf5d4af62e8b0b07f928a22c1afe532b38cf
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-03-03 10:05:26 +0200

    Extract the logic of opening new pdf file into PdfOpener class

---
+++ DrawboardPDFApp/Services/IPdfOpener.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace DrawboardPDFApp.Services
+{
+    public interface IPdfOpener
+    {
+        Task OpenExistingFileAsync(StorageFile file);
+        Task OpenNewFileAsync();
+    }
+}
---
+++ DrawboardPDFApp/Services/PdfOpener.cs
@@ -0,0 +1,41 @@
+﻿using DrawboardPDFApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace DrawboardPDFApp.Services
+{
+    public class PdfOpener : IPdfOpener
+    {
+        private readonly ITabViewService tabViewService;
+        private readonly IPdfFileOpenPicker pdfFileOpenPicker;
+        private readonly IOpenedFilesHistoryKeeper openedFilesHistoryKeeper;
+
+        public PdfOpener(ITabViewService tabViewService, IPdfFileOpenPicker pdfFileOpenPicker, IOpenedFilesHistoryKeeper openedFilesHistoryKeeper)
+        {
+            this.tabViewService = tabViewService;
+            this.pdfFileOpenPicker = pdfFileOpenPicker;
+            this.openedFilesHistoryKeeper = openedFilesHistoryKeeper;
+        }
+
+        public async Task OpenNewFileAsync()
+        {
+            var file = await pdfFileOpenPicker.PickSingleFileAsync();
+            if (file is null)
+            {
+                return;
+            }
+
+            await OpenExistingFileAsync(file);
+        }
+
+        public async Task OpenExistingFileAsync(StorageFile file)
+        {
+            await openedFilesHistoryKeeper.RecordFileOpeningAsync(file);
+            tabViewService.AddTab(file.DisplayName, typeof(OpenedPdfView), file);
+        }
+    }
+}
