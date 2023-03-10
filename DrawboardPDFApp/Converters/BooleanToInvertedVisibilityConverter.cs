de03d4c4127ae3763e4c23017365b6ad9440df9b
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-03-10 14:54:47 +0200

    Add BooleanToInvertedVisibilityConverter

---
+++ DrawboardPDFApp/Converters/BooleanToInvertedVisibilityConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace DrawboardPDFApp.Converters
+{
+    public class BooleanToInvertedVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool boolValue)
+            {
+                switch (boolValue)
+                {
+                    case true: return Visibility.Collapsed;
+                    case false: return Visibility.Visible;
+                }
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Visibility visibility)
+            {
+                switch (visibility)
+                {
+                    case Visibility.Collapsed: return true;
+                    case Visibility.Visible: return false;
+                }
+            }
+            return value;
+        }
+    }
+}
